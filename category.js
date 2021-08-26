d1e4220ffc272d164fd144d6f08ee0af8f9b8860
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-08-26 08:03:13 +0530

    category files

---
+++ category.js
@@ -0,0 +1,156 @@
+//URL: https://www.shoppersstop.com/men-clothing-t-shirts-polos/c-A101010
+
+//Product database to store in local storage:
+​
+const tshirts = [
+    {   
+        id: "1",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.5",
+        sizes: ["MEDIUM", "X-LARGE", "XX-LARGE"],
+        color: "White",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h5f/h81/16536303632414/S21T-01BA-040WH_WHITE.jpg_230Wx334H", "https://sslimages.shoppersstop.com/sys-master/images/h8f/h7a/16536303435806/S21T-01BA-040WH_WHITE.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h9b/h52/16536416387102/S21T-01BA-040WH_WHITE_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h11/h67/16536890966046/S21T-01BA-040WH_WHITE_alt2.jpg_1088Wx1632H"]
+    },
+    {
+        id: "2",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    },
+    {
+        id: "3",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+    },
+    {
+        id: "4",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    },
+    {
+        id: "5",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+    },
+    {
+        id: "6",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    },
+    {
+        id: "7",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+    },
+    {
+        id: "8",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    },
+    {
+        id: "9",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+    },
+    {
+        id: "10",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    },
+    {
+        id: "11",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+    },
+    {
+        id: "12",
+        name: "SPYKAR",
+        description: "Solid Cotton Slim Fit Mens T-Shirt",
+        price: "999",
+        discount: "30",
+        brand: "SPYKAR",
+        rating: "4.2",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
+        color: "Coral",
+        images: ["","",""]
+
+    }
+]
