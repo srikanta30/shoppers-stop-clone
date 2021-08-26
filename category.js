b5a46dbb66b1de00119a79b5b0b19af516625818
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-08-26 08:22:42 +0530

    updated database

--- category.js
+++ category.js
@@ -10,7 +10,7 @@ const tshirts = [
         price: "999",
         discount: "30",
         brand: "SPYKAR",
-        rating: "4.5",
+        rating: "4.1",
         sizes: ["MEDIUM", "X-LARGE", "XX-LARGE"],
         color: "White",
         images: ["https://sslimages.shoppersstop.com/sys-master/images/h5f/h81/16536303632414/S21T-01BA-040WH_WHITE.jpg_230Wx334H", "https://sslimages.shoppersstop.com/sys-master/images/h8f/h7a/16536303435806/S21T-01BA-040WH_WHITE.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h9b/h52/16536416387102/S21T-01BA-040WH_WHITE_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h11/h67/16536890966046/S21T-01BA-040WH_WHITE_alt2.jpg_1088Wx1632H"]
@@ -22,60 +22,60 @@ const tshirts = [
         price: "999",
         discount: "30",
         brand: "SPYKAR",
-        rating: "4.2",
-        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
-        color: "Coral",
-        images: ["","",""]
+        rating: "4.5",
+        sizes: ["LARGE", "MEDIUM", "X-LARGE", "XX-LARGE"],
+        color: "Ink Blue",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/hd2/h7e/16536835227678/S21T-01BA-039IN_INK_BLUE.jpg_230Wx334H", "https://sslimages.shoppersstop.com/sys-master/images/h8f/h85/16536835031070/S21T-01BA-039IN_INK_BLUE.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h6e/hb4/16536546541598/S21T-01BA-039IN_INK_BLUE_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h36/h89/16537036095518/S21T-01BA-039IN_INK_BLUE_alt2.jpg_1088Wx1632H"]
 
     },
     {
         id: "3",
-        name: "SPYKAR",
-        description: "Solid Cotton Slim Fit Mens T-Shirt",
-        price: "999",
-        discount: "30",
-        brand: "SPYKAR",
-        rating: "4.2",
-        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
-        color: "Coral",
-        images: ["","",""]
+        name: "ALTLIFE",
+        description: "Printed Cotton Regular Fit Mens T-Shirt",
+        price: "599",
+        discount: "50",
+        brand: "ALTLIFE",
+        rating: "4.3",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE", "XXX-LARGE"],
+        color: "Black",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h62/hf5/16911277588510/S21183GPT472B_BLACK.jpg_230Wx334H", "https://sslimages.shoppersstop.com/sys-master/images/h10/hfd/16911277195294/S21183GPT472B_BLACK.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h45/h86/16911242428446/S21183GPT472B_BLACK_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/h52/h28/16911172763678/S21183GPT472B_BLACK_alt2.jpg_1088Wx1632H"]
     },
     {
         id: "4",
-        name: "SPYKAR",
-        description: "Solid Cotton Slim Fit Mens T-Shirt",
-        price: "999",
-        discount: "30",
-        brand: "SPYKAR",
-        rating: "4.2",
+        name: "LIFE",
+        description: "Printed Cotton Blend Regular Fit Mens T-Shirt",
+        price: "599",
+        discount: "50",
+        brand: "LIFE",
+        rating: "4.6",
         sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
-        color: "Coral",
-        images: ["","",""]
+        color: "Oatmeal",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h30/h09/16383076991006/S21183RNP461OT_OATMEAL.jpg_230Wx334H","https://sslimages.shoppersstop.com/sys-master/images/h00/h10/16383076794398/S21183RNP461OT_OATMEAL.jpg_1088Wx1632H","https://sslimages.shoppersstop.com/sys-master/images/ha5/h65/16383098814494/S21183RNP461OT_OATMEAL_alt1.jpg_1088Wx1632H","https://sslimages.shoppersstop.com/sys-master/images/h69/hf5/16383113560094/S21183RNP461OT_OATMEAL_alt2.jpg_1088Wx1632H"]
 
     },
     {
         id: "5",
-        name: "SPYKAR",
-        description: "Solid Cotton Slim Fit Mens T-Shirt",
-        price: "999",
-        discount: "30",
-        brand: "SPYKAR",
-        rating: "4.2",
-        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
-        color: "Coral",
-        images: ["","",""]
+        name: "INDIAN TERRAIN",
+        description: "Printed Cotton Stretch Regular Fit Mens T-Shirt",
+        price: "1599",
+        discount: "40",
+        brand: "INDIAN TERRAIN",
+        rating: "4.9",
+        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "XX-LARGE"],
+        color: "Blue",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h74/hbe/16732900982814/S21123TS00927IN_BLUE.jpg_230Wx334H","https://sslimages.shoppersstop.com/sys-master/images/hf6/hb6/16732900720670/S21123TS00927IN_BLUE.jpg_1088Wx1632H","https://sslimages.shoppersstop.com/sys-master/images/he4/hb3/16732838723614/S21123TS00927IN_BLUE_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/hc2/h2b/16732534767646/S21123TS00927IN_BLUE_alt2.jpg_1088Wx1632H"]
     },
     {
         id: "6",
-        name: "SPYKAR",
-        description: "Solid Cotton Slim Fit Mens T-Shirt",
-        price: "999",
-        discount: "30",
-        brand: "SPYKAR",
+        name: "LEE COOPER",
+        description: "Printed Cotton Regular Fit Mens T-Shirt",
+        price: "799",
+        discount: "50",
+        brand: "LEE COOPER",
         rating: "4.2",
-        sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
-        color: "Coral",
-        images: ["","",""]
+        sizes: ["MEDIUM", "SMALL", "X-LARGE", "XX-LARGE"],
+        color: "Grey",
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h8e/h6d/16978380324894/S21T44GRE_GREY.jpg_230Wx334H","https://sslimages.shoppersstop.com/sys-master/images/hbe/h66/16978380128286/S21T44GRE_GREY.jpg_1088Wx1632H","https://sslimages.shoppersstop.com/sys-master/images/hcc/h05/16978355093534/S21T44GRE_GREY_alt1.jpg_1088Wx1632H", "https://sslimages.shoppersstop.com/sys-master/images/hf4/h0c/16978460213278/S21T44GRE_GREY_alt2.jpg_1088Wx1632H"]
 
     },
     {
@@ -88,7 +88,7 @@ const tshirts = [
         rating: "4.2",
         sizes: ["LARGE", "MEDIUM", "SMALL", "X-LARGE", "X-SMALL", "XX-LARGE"],
         color: "Coral",
-        images: ["","",""]
+        images: ["https://sslimages.shoppersstop.com/sys-master/images/h1b/hb8/16313389482014/206890662_MID_BLUE.jpg_230Wx334H","","",""]
     },
     {
         id: "8",
