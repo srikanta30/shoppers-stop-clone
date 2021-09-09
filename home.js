f965747882daaec4e5654b4bb4341ffc4618c794
huxly123 <huxlygerard5@gmail.com>
2021-09-09 17:25:53 +0530

    sticky

--- home.js
+++ home.js
@@ -39,23 +39,6 @@ function changeImg(){
 }
 window.onload=changeImg
 //settimeout
-//header
-window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-//header
 
 
